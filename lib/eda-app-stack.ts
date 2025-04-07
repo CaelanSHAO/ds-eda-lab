001a87fc6b659dac096339d076d11aa216116f57
JIAYI SHAO <sjy08015342@163.com>
2025-04-07 09:47:26 +0100

    Completed Fan out implementation - added emailing logic

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -64,6 +64,45 @@ export class EDAAppStack extends cdk.Stack {
 
     imagesBucket.grantRead(processImageFn);
 
+    // 第二部分：添加 SES 服务实现邮件发送
+
+    // 7. 创建新的 SQS 队列，用于邮件处理
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
+    // 8. 将新的队列添加为 SNS 主题的订阅者
+    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+    // 9. 创建新的 Lambda 函数，用于发送邮件（SES 调用）
+    const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    });
+
+    // 10. 配置新的 Lambda 函数的事件源为 mailerQ
+    const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(5),
+    });
+    mailerFn.addEventSource(newImageMailEventSource);
+
+    // 11. 授予 mailerFn 使用 SES 服务发送邮件的权限
+    mailerFn.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "ses:SendEmail",
+          "ses:SendRawEmail",
+          "ses:SendTemplatedEmail",
+        ],
+        resources: ["*"],
+      })
+    );
+
+
 
     // Output
 
